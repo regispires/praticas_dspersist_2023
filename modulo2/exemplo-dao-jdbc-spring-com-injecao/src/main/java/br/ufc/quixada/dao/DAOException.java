b8100e8ecba7219b13f04539181c93c0bfded049
Regis Pires Magalhaes <regis@dmz.org.br>
2023-10-18 03:57:41 -0300

    added exemplo-dao-jdbc-spring-com-injecao and sqlite db

---
+++ modulo2/exemplo-dao-jdbc-spring-com-injecao/src/main/java/br/ufc/quixada/dao/DAOException.java
@@ -0,0 +1,21 @@
+package br.ufc.quixada.dao;
+
+public class DAOException extends RuntimeException {
+
+	public DAOException() {
+		super();
+	}
+
+	public DAOException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public DAOException(String message) {
+		super(message);
+	}
+
+	public DAOException(Throwable cause) {
+		super(cause);
+	}
+
+}
